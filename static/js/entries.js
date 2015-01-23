19ea42428b551a78218a459850a674b6326ae118
Daniel Kats <dbkats@gmail.com>
2015-01-22 19:45:21 -0500

    restart done properly

--- static/js/entries.js
+++ static/js/entries.js
@@ -4,7 +4,6 @@ var maxVal = 5 * 60;
 
 function showHidePass(event) {
     var elem = $(event.target).parent().find(".password");
-    console.log(elem);
     var t = elem.attr("type");
     if (t === "password") {
         elem.attr("type", "text");
@@ -15,12 +14,17 @@ function showHidePass(event) {
     }
 }
 
+function resetLogoutTimer() {
+    restart = true;
+    console.log("restart");
+}
+
 function startLogoutTimer() {
     if (restart) {
         val = maxVal;
         restart = false;
     } else {
-        if (val === 0) {
+        if (val <= 0) {
             // logout
             window.location.href = "/logout";
         } else {
