be2250e242810956fc097a0a59835ce8464ceada
Daniel Kats <dbkats@cs.toronto.edu>
2015-12-08 22:28:56 -0500

    better unit test teardown and some testing for entries

--- api_test.py
+++ api_test.py
@@ -4,6 +4,8 @@ import unittest
 import requests
 import passzero
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import NoResultFound
+from requests import Request
 
 """
 Very hacky API testing - runs on local server
@@ -23,7 +25,17 @@ class PassZeroApiTester(unittest.TestCase):
         return "https://127.0.0.1:5050"
 
     def setUp(self):
+        # disable certificate warnings for testing
         requests.packages.urllib3.disable_warnings()
+
+    def tearDown(self):
+        email = "sample@fake.com"
+        try:
+            user = passzero.get_account_with_email(email)
+            assert user is not None
+            passzero.delete_account(user)
+        except NoResultFound:
+            pass
 
     def test_wrong_login(self):
         result = json_post(
@@ -71,6 +83,30 @@ class PassZeroApiTester(unittest.TestCase):
         )
         assert result is not None
         assert result.status_code == 200
+        passzero.delete_account(user)
+
+    def test_get_entries(self):
+        email = "sample@fake.com"
+        password = "right_pass"
+        user = passzero.create_inactive_account(email, password)
+        passzero.activate_account(user)
+        with requests.Session() as s:
+            headers = { "Content-Type": "application/json" }
+            data={
+                "email": email,
+                "password": password
+            }
+            auth_response = s.post(self.base_url + "/api/login",
+                data=json.dumps(data), headers=headers,
+                verify=False)
+            assert auth_response is not None
+            assert auth_response.status_code == 200
+            entry_response = s.get(self.base_url + "/api/entries",
+                headers=headers, verify=False)
+            assert entry_response is not None
+            assert entry_response.status_code == 200
+            assert entry_response.json() is not None
+            assert entry_response.json() == []
         passzero.delete_account(user)
 
 
