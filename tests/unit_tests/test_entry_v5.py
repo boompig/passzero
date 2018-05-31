3e3b7649a6fdc6f7f34af43b8170b4d32a8d5680
Daniel Kats <boompigdev@gmail.com>
2018-05-30 23:43:13 -0700

    test entry encryption and decryption when using a password with unicode chars

--- tests/unit_tests/test_entry_v5.py
+++ tests/unit_tests/test_entry_v5.py
@@ -1,5 +1,3 @@
-import six
-
 from passzero.models import Entry_v5
 
 
@@ -20,3 +18,19 @@ def test_encrypt_and_decrypt_entry_v5():
         assert dec_entry_out[field] == dec_entry_in[field]
 
 
+def test_encrypt_and_decrypt_entry_v5_utf_password():
+    """Make sure we can use some non-ASCII characters in the password"""
+    dec_entry_in = {
+        u"account": u"test account",
+        u"username": u"test username",
+        u"password": u"test password",
+        u"extra": u"test extra",
+        u"has_2fa": True
+    }
+    user_key = u"你好Sträfchen"
+    entry = Entry_v5()
+    entry.encrypt(user_key, dec_entry_in)
+    assert entry.version == 5
+    dec_entry_out = entry.decrypt(user_key)
+    for field in dec_entry_in:
+        assert dec_entry_out[field] == dec_entry_in[field]
