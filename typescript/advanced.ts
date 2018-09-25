20f032e53a7e94c17ccec0fea5006b0cc3e960ea
Daniel Kats <boompigdev@gmail.com>
2018-09-25 15:13:34 -0400

    replace inline javascript in advanced page with external javascript as part of CSP rewrite

--- typescript/advanced.ts
+++ typescript/advanced.ts
@@ -2,55 +2,10 @@
 /// <reference path="./passzero_api.ts" />
 /// <reference path="./utils.ts" />
 
-
 // module imports (tsc doesn't like these)
 //import * as $ from "jquery";
 //import { Utils } from "./utils";
 //import { pzAPI } from "./passzero_api";
-
-
-interface IChangeAccountPasswordData {
-	old_password: string;
-	new_password: string;
-	confirm_new_password: string;
-}
-
-function changePassword(e: Event) {
-	"use strict";
-	e.preventDefault();
-
-	const elem = $(e.target);
-	const dataArray = elem.serializeArray();
-	const data = Utils.parseArray(dataArray) as IChangeAccountPasswordData;
-
-	$(".form-error").text("");
-	$(".error-msg").text("");
-	$(".alert-success").hide();
-
-	pzAPI.changeAccountPassword(data.old_password, data.new_password, data.confirm_new_password)
-	.done((response) => {
-		$(".alert-success").text("Successfully changed password").show();
-		// reset form fields
-		elem.find("input[type='password']").each((idx) => {
-			$(this).val("");
-		});
-	})
-	.catch((obj, textStatus, textCode) => {
-		const response = obj.responseJSON;
-		$(".error-msg").text(response.msg);
-
-		for (const key in response) {
-			if (key !== "status" && key !== "msg") {
-				$("#form-error-" + key).text(response[key]);
-			}
-		}
-		console.log(obj);
-		console.log(textStatus);
-		console.log(textCode);
-	});
-
-	return false;
-}
 
 function nukeEntries(e: Event) {
 	"use strict";
@@ -64,4 +19,11 @@ function nukeEntries(e: Event) {
 	return false;
 }
 
-//export { changePassword, nukeEntries };
+$(() => {
+	const elem = document.querySelector("#nuke-entries-form");
+	if (elem) {
+		elem.addEventListener("submit", nukeEntries);
+	}
+});
+
+//export { nukeEntries };
